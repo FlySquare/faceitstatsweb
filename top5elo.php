bdc66b5e515c903306d58e22ead30159bbb7d70e
flysquare <umutkonurinso@gmail.com>
2020-05-20 01:08:53 +0300

    web server update

---
+++ top5elo.php
@@ -0,0 +1,72 @@
+<?php
+include('db/vericek.php');
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="aBQCWe1TD3DL8cSIWKTzXtmu7OmJymZBOgf4aDM3">
+
+    <title>FaceitElo.net - Check your Faceit Elo and Faceit Stats.</title>
+    <meta name="description" content="FaceitElo.net is a place where you check your Faceit elo and Faceit Stats completely for free. Check your Level, Elo points, Matches history, Ban history and other statistics.">
+    <meta property="og:title" content="FaceitElo.net - Check your Faceit Elo and Faceit Stats." />
+    <meta property="og:url" content="https://faceitelo.net"/>
+    <meta property="og:description" content="FaceitElo.net is a place where you check your Faceit elo and Faceit Stats completely for free. Check your Level, Elo points, Matches history, Ban history and other statistics."/>
+    <meta property="og:image" content="https://faceitelo.net/img/header.jpg" />
+    <meta property="og:site_name" content="faceitelo.net"/>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://faceitelo.net/css/flag-icon.min.css">
+
+
+    <link rel="stylesheet" href="https://faceitelo.net/css/bootstrap.min.css">
+    <link href="https://faceitelo.net/css/style.css" rel="stylesheet">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+</head>
+<body>
+    <div id="app">
+
+
+        <main>
+
+
+    <section class="section grey">
+    <?php
+    echo $namedizi13[0];
+     ?>
+</section>
+                                                      </main>
+
+
+    </div>
+</body>
+<!-- Scripts -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://faceitelo.net/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<script>
+  player_base_url = 'https://faceitelo.net/player' + '/';
+</script>
+<script src="https://faceitelo.net/js/script.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128566885-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128566885-1');
+</script>
+</html>
